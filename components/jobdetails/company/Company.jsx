23b8ab3cfaa3761a4f1b7bbbce2865c50db0bd99
aidan.newell <acnewell09@gmail.com>
2023-05-07 10:42:15 -0500

    company details component

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, Image } from "react-native";
 
-import styles from './company.style'
+import styles from "./company.style";
+import { icons } from "../../../constants";
+import { checkImageURL } from "../../../utils";
 
-const Company = () => {
+const Company = ({ companyLogo, jobTitle, companyName, location }) => {
   return (
-    <View>
-      <Text>Company</Text>
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+        <Image
+          source={{
+            uri: checkImageURL(companyLogo),
+          }}
+        />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Company
+export default Company;
