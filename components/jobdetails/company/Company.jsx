183ce61f7fbfcfb45963faf723b5c9058157a7ce
aidan.newell <acnewell09@gmail.com>
2023-05-07 10:53:33 -0500

    company details page template done

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -11,9 +11,27 @@ const Company = ({ companyLogo, jobTitle, companyName, location }) => {
       <View style={styles.logoBox}>
         <Image
           source={{
-            uri: checkImageURL(companyLogo),
+            uri: checkImageURL(companyLogo)
+              ? companyLogo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
+          style={styles.logoImage}
         />
+      </View>
+      <View style={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
+
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{companyName} /</Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            resizeMode="contain"
+            style={styles.locationImage}
+          />
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
       </View>
     </View>
   );
